54e1b8baf0288e0cc494989d34aca872eb2223ef
Matej Kosec <mkosec@stanford.edu>
2018-02-26 20:19:15 -0800

    Working slighlty better now 0.0050

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ class Config(object):
     """Holds model hyperparams and data information. Copied from 224n"""
     
     batch_size = 64
-    n_epochs = 10
-    lr = 0.05
-    n_test_samples = 3
-    results_dir='./experiments/base_model/'
+    n_epochs = 6
+    lr = 0.1
+    n_test_samples = 5
+    results_dir='./experiments/deconvolution_model/'
     
--- model.py
+++ model.py
@@ -32,29 +32,28 @@ class Model(object):
         """Implements the core of the model that transforms a batch of input data into predictions. """
         
         #Use convolution to encode the imput image
-        x1 = tf.contrib.layers.conv2d(self.input_frame_placeholder, 4, \
+        x1 = tf.contrib.layers.conv2d(self.input_frame_placeholder, 8, \
                                           kernel_size=[4,4], stride=(1,1), padding='SAME', normalizer_fn=tf.contrib.layers.batch_norm)
         print('x1 shape', x1.shape)
-        #x2 = tf.contrib.layers.max_pool2d(x1, \
-        #                                  kernel_size=[2,2], stride=(2,2), padding='SAME')
-        x2 = x1
+        x2 = tf.contrib.layers.max_pool2d(x1, \
+                                          kernel_size=[2,2], stride=(2,2), padding='SAME')
         print('x2 shape', x2.shape)
         x3 = tf.contrib.layers.conv2d(x2, 4, \
                                       kernel_size=[4,4], stride=(1,1), padding='SAME', normalizer_fn=tf.contrib.layers.batch_norm)
         print('x3 shape', x3.shape)
-        filter1 = tf.get_variable('filter', shape=[1,1,4,1],dtype=tf.float32)
-        x4 = tf.nn.conv2d(x3, filter=filter1, strides=[1,1,1,1], padding='SAME')
+        x4 = tf.contrib.layers.max_pool2d(x3, \
+                                          kernel_size=[2,2], stride=(2,2), padding='SAME')
         print('x4 shape', x4.shape)
-        #Now combine the encoding the the actoin vector
-        x5 =tf.contrib.layers.flatten(x4)
+        #Predict the future based on image encoding
+        x5 = tf.image.resize_bilinear(x4, [32,32])
         print('x5 shape', x5.shape)
-        x6 =self.input_action_placeholder
+        x6= tf.contrib.layers.conv2d_transpose(x5, 4 , [4,4], stride=1, padding='SAME')
         print('x6 shape', x6.shape)
-        x7 =tf.concat([x5,x6],axis=1)
+        x7 = tf.image.resize_bilinear(x6, [64,64])
         print('x7 shape', x7.shape)
-        
-        #Predict the future based on image encoding and action taken
-        pred = tf.contrib.layers.fully_connected(x7, 4096, activation_fn=tf.nn.sigmoid)
+        x8= tf.contrib.layers.conv2d_transpose(x7, 1 ,  [2,2], stride=1, padding='SAME',activation_fn=tf.nn.sigmoid)
+        print('x8 shape', x8.shape)
+        pred=tf.contrib.layers.flatten(x8)
         print('pred shape', pred.shape)
             
 
@@ -107,14 +106,14 @@ class Model(object):
             print('Predictions shape:', predictions.shape)
             print('Prediction shape:', prediction.shape)
             
-            plt.figure(1,figsize=(20,10))
+            plt.figure(1,figsize=(10,5))
             plt.subplot(121)
             plt.imshow(prediction.reshape((64,64)),cmap="Greys")
             plt.xlabel('Prediction') #1
             plt.subplot(122)
             plt.imshow(truth.reshape((64,64)),cmap="Greys")
             plt.xlabel('Truth')
-            plt.savefig('./data/example_figure%i.png'%i,dpi=300)
+            plt.savefig('./data/example_figure%i.png'%i,dpi=300, bbox_inches='tight')
         
     def run_epoch(self, sess, train_buffer, dev_buffer):
         n_minibatches = train_buffer.num_in_buffer // self.config.batch_size
